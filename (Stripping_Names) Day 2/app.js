3c8a15382458b05e738c388dd00f9cb3e617cff9
mrfaizee12 <160962821+mrfaizee12@users.noreply.github.com>
2024-03-04 23:30:44 +0500

    Add files via upload

---
+++ (Stripping_Names) Day 2/app.js
@@ -0,0 +1,7 @@
+"use strict";
+/* Task #06 :Stripping Names: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+*/
+let Namewithwhitespace = "\t\n FAIZAN \t\n";
+console.log(Namewithwhitespace);
+let Namewithoutwhitespace = Namewithwhitespace.trim();
+console.log(Namewithoutwhitespace);
