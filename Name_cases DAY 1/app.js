6416aaea5d403491ce8a49090e843c697835f995
mrfaizee12 <160962821+mrfaizee12@users.noreply.github.com>
2024-03-04 19:51:47 +0500

    Add files via upload

---
+++ Name_cases DAY 1/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let personName = "Faizan Anjum";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.replace(/\b\w/g, (char) => char.toUpperCase()));
---
+++ Name_cases DAY 1/app.ts
@@ -0,0 +1,4 @@
+let personName: string ="Faizan Anjum";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.replace (/\b\w/g, (char) => char.toUpperCase()));
