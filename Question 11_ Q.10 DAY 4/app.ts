fab7f456464dca797059d43f7f68bbb76e4ce842
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2024-03-06 20:14:14 +0500

    Add files via upload

---
+++ Question 11_ Q.10 DAY 4/app.ts
@@ -0,0 +1,14 @@
+/* Question 11: Names: Store the names of a few of your friends in an array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+Question 10 (part 2): Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.*/
+
+//Faizan Anjum
+//march-06-2024
+ 
+let names: string[] = ["Faizan", "Aziz", "Umar", "Hassan"];
+
+//printing names one by one
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
