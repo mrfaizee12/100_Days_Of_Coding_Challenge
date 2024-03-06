ec675195629c3dc4326221b306415d7131f11578
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2024-03-06 20:12:53 +0500

    Add files via upload

---
+++ Task #09 + Task # 10 (part_1)/app.js
@@ -0,0 +1,7 @@
+"use strict";
+/* Task # 9 : Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+Task # 10 (Part 1) : Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.*/
+const Favorite_Number = 99;
+const Message = (`My favorite number is , ${Favorite_Number}`);
+console.log(Message);
---
+++ Task #09 + Task # 10 (part_1)/app.ts
@@ -0,0 +1,12 @@
+/* Task # 9 : Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+Task # 10 (Part 1) : Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.*/
+
+//FAIZAN ANJUM
+//thursday, feb-29-2024
+
+//store my favorite number in a variable
+const Favorite_Number : number = 99;
+//revealing my favorite number
+const Message : string =( `My favorite number is , ${Favorite_Number}` );
+console.log(Message);
