9bdd2f56e4277109097695e273bb531f6e99fd72
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2024-03-08 22:48:27 +0500

    Add files via upload

---
+++ More Guest_Day 6/app.js
@@ -0,0 +1,14 @@
+"use strict";
+// Question 16: More Guests: You've found a bigger dinner table, so there's room for more guests.
+let guestList = ["Robert", "Romen", "Rolin"];
+console.log("\nNEW LIST OF INVITATION: \n");
+for (let j = 0; j < guestList.length; j++) {
+    console.log(`Dear ${guestList[j]}: \n you are invited to dinner! \n`);
+}
+console.log("Good news! We've found a bigger dinner table, so more space is available. So our new guests are: \n");
+guestList.unshift("Cena");
+guestList.splice(2, 0, "James");
+guestList.push("John");
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Dear ${guestList[i]}: \n you are invited to dinner! \n`);
+}
