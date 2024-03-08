645c869d29a713528a1b20adae33033df66a4bb0
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2024-03-08 22:49:48 +0500

    Add files via upload

---
+++ Question 18 _ Day 6/app.ts
@@ -0,0 +1,22 @@
+// Question 18: Seeing the World: Think of at least five places you’d like to visit.
+
+let contries: string[] = ["Turkey", "China", "Farance", "Italy"];
+console.log("Original Order:", contries);
+
+console.log("Alphabetical Order:",[...contries].sort());
+
+console.log("Still in Original Order:", contries);
+
+console.log("Reverse Order:", [...contries].reverse());
+
+console.log("Still in Original Order:", contries);
+
+// we have Changed the original Array now
+console.log("Original Array Revered:",contries.reverse());
+
+console.log("Back to Original Order:", contries.reverse());
+
+console.log("Sorted in Alphabetical Order:", contries.sort());
+
+// we have Changed again the original Array now
+console.log("Original Array Revered Again:",contries.reverse());
