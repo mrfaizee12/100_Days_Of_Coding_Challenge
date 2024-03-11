d287ab3f4e87e108c7f8cf650fc456d1f948c38d
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2024-03-11 19:16:52 +0500

    Add files via upload

---
+++ Question 21_Day 7/app.js
@@ -0,0 +1,13 @@
+"use strict";
+// Question 21: Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let university = { uniName: "Karachi University",
+    department: "Islamic Learning",
+    noOfStudent: 30000
+};
+console.log(`University Info: ${university.uniName}, Departmen of ${university.department}, number of students ${university.noOfStudent}`);
+// let book: { title: string; author: string; yearPublished: number } = {
+// title: "The Hobbit",
+// author: "J.R.R. Tolkien",
+// yearPublished: 1937
+// };
+// console.log(`Book Info: ${book.title} by ${book.author}, published in ${book.yearPublished}`);
---
+++ Question 21_Day 7/app.ts
@@ -0,0 +1,15 @@
+// Question 21: Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let university: {
+    uniName: string;
+     department: string; 
+     noOfStudent: number
+} = {uniName: "Karachi University",
+department: "Islamic Learning",
+noOfStudent: 30000
+};
+
+console.log(`University Info: ${university.uniName}, Departmen of ${university.department}, number of students ${university.noOfStudent}`);
+
+
+
