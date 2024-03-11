13991baab878d794b691f4d15aa929c5cd7b9ea1
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2024-03-11 19:16:15 +0500

    Add files via upload

---
+++ Question 20_Day 7/app.js
@@ -0,0 +1,4 @@
+"use strict";
+// Question 20: Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let cricketPlayers = ["Babar Azam", "Vrat Kohli", "Steve Smith", "Kane Williamson"];
+console.log("Players I'd like to meet:", cricketPlayers);
---
+++ Question 20_Day 7/app.ts
@@ -0,0 +1,5 @@
+// Question 20: Think of something you could store in an array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let cricketPlayers: string[] = ["Babar Azam", "Vrat Kohli", "Steve Smith", "Kane Williamson"];
+
+console.log("Players I'd like to meet:", cricketPlayers);
