e75cdb1821f089c0eec2730983d0793835e978e7
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2024-03-11 19:18:18 +0500

    Add files via upload

---
+++ Conditional Test_Day 8/q23.js
@@ -0,0 +1,25 @@
+"use strict";
+// Question 23: Conditional Tests: Write a series of conditional tests. Predict the results of each test.
+let two = 2;
+let four = 4;
+let six = 6;
+console.log("Test 1: Two is equal to 2?");
+console.log(two == 2);
+console.log("Test 2: Four is equal to 4?");
+console.log(four == 4);
+console.log("\nTest 3: Six is not equal to 4?");
+console.log(six != 4);
+console.log("\nTest 4: Four is smaller than 6?");
+console.log(four < 6);
+console.log("\nTest 5: Four is greater than 2?");
+console.log(four > 2);
+console.log("\nTest 6: Six is smaller than 4?");
+console.log(six < 4);
+console.log("\nTest 7:Two is equal to four?");
+console.log(two == 4);
+console.log("\nTest 8: Two is not equal to 2?");
+console.log(two != 2);
+console.log("\nTest 9: Two is greater than 4?");
+console.log(two > 4);
+console.log("\nTest 10: Four is equal to 100?");
+console.log(four == 100);
