01b5512de487ce788820112d49e2d4895ebfcca5
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2024-03-11 19:15:27 +0500

    Add files via upload

---
+++ Question 19 _ Day 7/app.js
@@ -0,0 +1,5 @@
+"use strict";
+// Question 19: Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
+let guestList = ["Cena", "Robert"];
+let count_guestList = guestList.length;
+console.log(`${count_guestList} People are coming to the dinner`);
---
+++ Question 19 _ Day 7/app.ts
@@ -0,0 +1,6 @@
+// Question 19: Dinner Guests: Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
+
+let guestList: string[] =["Cena", "Robert"]
+let count_guestList: number = guestList.length
+
+console.log(`${count_guestList} People are coming to the dinner`)
