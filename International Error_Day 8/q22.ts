d4099b0849a65203f48076a4c8ac012f11d9067f
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2024-03-11 19:17:49 +0500

    Add files via upload

---
+++ International Error_Day 8/q22.ts
@@ -0,0 +1,8 @@
+// Question 22: Intentional Error: Try to produce an array index error in one of your programs. Correct the error before finishing.
+
+let students: string[] =["Aamir", "Munir", "Raza", "Usama"];
+
+//producing error by Accessing invalid index of Array
+// console.log(students[4]);
+ //error removed
+console.log(students[1]);
