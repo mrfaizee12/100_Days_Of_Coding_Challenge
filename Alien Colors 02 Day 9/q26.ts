e4ef79815440a09c90e6e12fcac56f688e4823b0
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2024-03-13 02:20:07 +0500

    Add files via upload

---
+++ Alien Colors 02 Day 9/q26.ts
@@ -0,0 +1,19 @@
+// Question 26: Alien Colors #2: Choose a color for an alien, then write an if-else chain.
+// 1st version
+let alienColor = "green";
+
+if (alienColor === "green"){
+    console.log("he just earned 5 points for shooting the alien")
+}
+else{
+    console.log("he just earned 10 points");
+}
+
+// 2nd version
+
+if (alienColor ==="black"){
+    console.log("yellow")
+}
+else{
+    console.log("he just earnrd 10 points");
+}
