eac8647e5cc060333f66d457c336fed1408e5fc6
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2024-03-13 02:19:40 +0500

    Add files via upload

---
+++ Alien Colors 01 Day 9/q25.js
@@ -0,0 +1,9 @@
+"use strict";
+// Question 25: Alien Colors #1: Imagine an alien was just shot down in a game. Assign 'green', 'yellow', or 'red' to a variable called alien_color.
+let alien_color = "green";
+if (alien_color === "green") {
+    console.log("you earn 5 points");
+}
+if (alien_color === "yellow") {
+    console.log("yellow");
+}
