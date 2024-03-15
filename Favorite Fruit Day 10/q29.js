6b8566bfc264ab491eba7984db16a7fb0e9a966d
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2024-03-16 03:50:20 +0500

    Add files via upload

---
+++ Favorite Fruit Day 10/q29.js
@@ -0,0 +1,18 @@
+"use strict";
+// Question 29: Favorite Fruit: Create an array for your favorite fruits and check if certain fruits are included.//
+let favoriteFruits = ["Melon", "Strawberry", "Mango"];
+if (favoriteFruits.includes("Melon")) {
+    console.log("I love melons");
+}
+if (favoriteFruits.includes("Strawberry")) {
+    console.log("I love strawberries");
+}
+if (favoriteFruits.includes("Guava")) {
+    console.log("I love guava");
+}
+if (favoriteFruits.includes("Mango")) {
+    console.log("I love mangoes");
+}
+if (favoriteFruits.includes("Peach")) {
+    console.log("I love peaches");
+}
---
+++ Favorite Fruit Day 10/q29.ts
@@ -0,0 +1,20 @@
+// Question 29: Favorite Fruit: Create an array for your favorite fruits and check if certain fruits are included.//
+
+let favoriteFruits: string[] =["Melon", "Strawberry", "Mango"];
+
+// using five Independent "if" statements
+if(favoriteFruits.includes("Melon")){
+    console.log("I love melons");
+}
+if(favoriteFruits.includes("Strawberry")){
+    console.log("I love strawberries");
+}
+if(favoriteFruits.includes("Guava")){
+    console.log("I love guava");
+}
+if(favoriteFruits.includes("Mango")){
+    console.log("I love mangoes");
+}
+if(favoriteFruits.includes("Peach")){
+    console.log("I love peaches");
+}
