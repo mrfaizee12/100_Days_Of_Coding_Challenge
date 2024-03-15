cf8b9b11b322640cf1907716afadaceb9fbbce0c
Faizan Anjum <160962821+mrfaizee12@users.noreply.github.com>
2024-03-16 03:49:53 +0500

    Add files via upload

---
+++ Stage of life Day 10/q28.js
@@ -0,0 +1,22 @@
+"use strict";
+// Question 28: Stages of Life: Determine a person’s life stage with an if-else chain.
+let age = 21;
+// Determining the stage of life using if-else chain
+if (age < 2) {
+    console.log("You are a baby.");
+}
+else if (age >= 2 && age < 4) {
+    console.log("You are a toddler.");
+}
+else if (age >= 4 && age < 13) {
+    console.log("You are a kid.");
+}
+else if (age >= 13 && age < 20) {
+    console.log("You are a teenagers.");
+}
+else if (age >= 20 && age < 65) {
+    console.log("You are an adults.");
+}
+else if (age >= 65) {
+    console.log("You are elders.");
+}
